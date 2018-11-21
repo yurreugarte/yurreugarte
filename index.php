70435b477ae18c36f1e457c01089b4a75b5edfed
Raúl <raul@adimedia.net>
2018-11-21 13:15:15 +0100

    Añadir un title a la plantilla

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
+    <title>Plantilla HTML 5 - Bootstrap 4</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
