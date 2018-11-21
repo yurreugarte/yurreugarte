7d7d83d897a2fc84a722ef6991f920b3c595bdbb
Raúl <raul@adimedia.net>
2018-11-21 16:02:10 +0100

    Añadir el directorio leeme

---
+++ AAA-LEEME/leeme.php
@@ -0,0 +1,10 @@
+<?php
+exit();
+
+/*
+
+¿QUÉ TENEMOS QUE TENER EN CUENTA EN ESTE PROYECTO?
+==================================================
+
+- Aunque no esté ignorado en el repositorio .git, no subiremos el directorio src y su contenido
+*/
